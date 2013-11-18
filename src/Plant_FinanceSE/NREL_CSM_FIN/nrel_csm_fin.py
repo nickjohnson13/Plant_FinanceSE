da6970f5fd9716441ba3e4bcff9255e8adff4cba
unknown <kdykes@kdykes-22302s.nrel.gov>
2013-11-18 13:17:34 -0700

    updated variable names

--- src/Plant_FinanceSE/NREL_CSM_FIN/nrel_csm_fin.py
+++ src/Plant_FinanceSE/NREL_CSM_FIN/nrel_csm_fin.py
@@ -24,7 +24,7 @@ class fin_csm_assembly(BaseFinancialModel):
         
         self.replace('fin', fin_csm_component())
         
-        self.create_passthrough('fin.rated_power')
+        self.create_passthrough('fin.machine_rating')
         self.create_passthrough('fin.fixed_charge_rate')
         self.create_passthrough('fin.construction_finance_rate')
         self.create_passthrough('fin.tax_rate')
@@ -32,9 +32,9 @@ class fin_csm_assembly(BaseFinancialModel):
         self.create_passthrough('fin.construction_time')
         self.create_passthrough('fin.project_lifetime')
         
-        self.create_passthrough('fin.preventative_maintenance_cost')
-        self.create_passthrough('fin.corrective_maintenance_cost')
-        self.create_passthrough('fin.land_lease_cost')
+        self.create_passthrough('fin.preventative_opex')
+        self.create_passthrough('fin.corrective_opex')
+        self.create_passthrough('fin.lease_opex')
         self.create_passthrough('fin.sea_depth')
         
         self.create_passthrough('fin.lcoe')
@@ -42,7 +42,7 @@ class fin_csm_assembly(BaseFinancialModel):
 class fin_csm_component(BaseFinancialAggregator):    
 
     # variables
-    rated_power = Float(5000.0, units = 'kW', iotype = 'in', desc = 'rated power for a wind turbine')
+    machine_rating = Float(5000.0, units = 'kW', iotype = 'in', desc = 'rated power for a wind turbine')
     
     # parameters
     fixed_charge_rate = Float(0.12, iotype = 'in', desc = 'fixed charge rate for coe calculation')
@@ -52,21 +52,21 @@ class fin_csm_component(BaseFinancialAggregator):
     construction_time = Float(1.0, iotype = 'in', desc = 'number of years to complete project construction')
     project_lifetime = Float(20.0, iotype = 'in', desc = 'project lifetime for LCOE calculation')
 
-    preventative_maintenance_cost = Float(401819.023, iotype='in', units='USD', desc='O&M costs')
-    corrective_maintenance_cost   = Float(91048.387, iotype='in', units='USD', desc='levelized replacement costs')
-    land_lease_cost     = Float(22225.395, iotype='in', units='USD', desc='land lease costs')  
+    preventative_opex = Float(401819.023, iotype='in', units='USD', desc='O&M costs')
+    corrective_opex   = Float(91048.387, iotype='in', units='USD', desc='levelized replacement costs')
+    lease_opex     = Float(22225.395, iotype='in', units='USD', desc='land lease costs')  
     sea_depth = Float(20.0, iotype='in', units='m', desc = 'depth of project for offshore, (0 for onshore)')
         
     # output
-    lcoe = Float(0.0, iotype='out', desc='Cost of energy - unlevelized')
+    lcoe = Float(0.0, iotype='out', desc='_cost of energy - unlevelized')
     
     def __init__(self):
         """
-        OpenMDAO component to wrap finance model of the NREL Cost and Scaling Model (csmFinance.py)
+        OpenMDAO component to wrap finance model of the NREL _cost and Scaling Model (csmFinance.py)
 
         Parameters
         ----------  
-		    ratedPower : float
+		    machine_rating : float
 		      rated power for a wind turbine [kW]
 		    fixedChargeRate : float
 		      fixed charge rate for coe calculation
@@ -78,31 +78,31 @@ class fin_csm_component(BaseFinancialAggregator):
 		      applicable project discount rate
 		    constructionTime : float
 		      number of years to complete project construction
-		    projectLifetime : float
+		    project_lifetime : float
 		      project lifetime for LCOE calculation
-		    turbineNumber : int
+		    turbine_number : int
 		      number of turbines at plant
 		    aep : float
 		      Annual energy production [kWh]
-		    turbineCost : float
+		    turbine_cost : float
 		      Turbine capital costs [USD per turbine]
 		    BOScost : float
 		      Balance of station costs total [USD]
-		    preventativeMaintenanceCost : float
+		    preventativeMaintenance_cost : float
 		      O&M costs annual total [USD]
-		    correctiveMaintenanceCost : float
+		    correctiveMaintenance_cost : float
 		      levelized replacement costs annual total [USD]
-		    landLeaseCost : float
+		    landLease_cost : float
 		      land lease costs annual total [USD] 
-		    seaDepth : float
+		    sea_depth : float
 		      depth of project [m]
 		        
         Returns
         -------
 		    coe : float
-		      Cost of energy - unlevelized [USD/kWh]
+		      _cost of energy - unlevelized [USD/kWh]
 		    lcoe : float
-		      Cost of energy - levelized [USD/kWh]
+		      _cost of energy - levelized [USD/kWh]
 
         """
 
@@ -114,13 +114,13 @@ class fin_csm_component(BaseFinancialAggregator):
 
     def execute(self):
         """
-        Executes finance model of the NREL Cost and Scaling model to determine overall plant COE and LCOE.
+        Executes finance model of the NREL _cost and Scaling model to determine overall plant COE and LCOE.
         """
         
         print "In {0}.execute()...".format(self.__class__)
 
-        self.fin.compute(self.rated_power, self.turbine_cost * self.turbine_number, self.preventative_maintenance_cost, \
-                         self.land_lease_cost, self.corrective_maintenance_cost, self.bos_cost, self.net_aep, \
+        self.fin.compute(self.machine_rating, self.turbine_cost * self.turbine_number, self.preventative_opex, \
+                         self.lease_opex, self.corrective_opex, self.bos_costs, self.net_aep, \
                          self.fixed_charge_rate, self.construction_finance_rate, self.tax_rate, self.discount_rate, self.construction_time, \
                          self.project_lifetime, self.turbine_number, self.sea_depth)
 
@@ -135,11 +135,11 @@ def example():
 
     fin.turbine_cost = 6087803.555 / 50
     fin.turbine_number = 50
-    fin.preventative_maintenance_cost = 401819.023
-    fin.land_lease_cost = 22225.395
-    fin.corrective_maintenance_cost = 91048.387
-    fin.avg_annual_opex = fin.preventative_maintenance_cost + fin.corrective_maintenance_cost + fin.land_lease_cost
-    fin.bos_cost = 7668775.3
+    fin.preventative_opex = 401819.023
+    fin.lease_opex = 22225.395
+    fin.corrective_opex = 91048.387
+    fin.avg_annual_opex = fin.preventative_opex + fin.corrective_opex + fin.lease_opex
+    fin.bos_costs = 7668775.3
     fin.net_aep = 15756299.843
     
     fin.execute()
