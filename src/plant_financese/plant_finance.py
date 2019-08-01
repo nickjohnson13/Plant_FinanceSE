392b5219ad75f24a64393c3170be45f910f06bb7
Johnson <njohnso1@nrel.gov>
2019-08-01 16:44:36 -0600

    updated LCOE calculations based on COE report

--- src/plant_financese/plant_finance.py
+++ src/plant_financese/plant_finance.py
@@ -1,5 +1,4 @@
 from openmdao.api import Component
-
 import numpy as np
 
 class PlantFinance(Component):
@@ -16,6 +15,8 @@ class PlantFinance(Component):
         self.add_param('park_aep',          val=0.0, units='kW*h',  desc='Annual Energy Production of the wind plant')
         self.add_param('turbine_aep',       val=0.0, units='kW*h',  desc='Annual Energy Production of the wind turbine')
         self.add_param('wake_loss_factor',  val=0.0,                desc='The losses in AEP due to waked conditions')
+        self.add_param('net_energy_capture',  val=0.0, units='MWh/MW/yr',                desc='total energy of the plant')
+        self.add_param('machine_rating',  val=0.0, units='MW',                desc='rating of the turbine')
 
         # parameters
         self.add_param('fixed_charge_rate', val=0.12,               desc = 'Fixed charge rate for coe calculation')
@@ -23,11 +24,12 @@ class PlantFinance(Component):
         self.add_param('discount_rate',     val=0.07,               desc = 'Applicable project discount rate')
         self.add_param('construction_time', val=1.0,  units='year', desc = 'Number of years to complete project construction')
         self.add_param('project_lifetime',  val=20.0, units='year', desc = 'Project lifetime for LCOE calculation')
-        self.add_param('sea_depth',         val=20.0, units='m',    desc = 'Sea depth of project for offshore, (0 for onshore)')
+        self.add_param('sea_depth',         val=0.0, units='m',    desc = 'Sea depth of project for offshore, (0 for onshore)')
 
         #Outputs
         self.add_output('lcoe',             val=0.0, units='USD/kW',desc='Levelized cost of energy for the wind plant')
         self.add_output('coe',              val=0.0, units='USD/kW',desc='Cost of energy for the wind plant - unlevelized')
+
         
         self.verbosity = verbosity
         
@@ -36,9 +38,9 @@ class PlantFinance(Component):
         # Unpack parameters
         depth       = params['sea_depth']
         n_turbine   = params['turbine_number']
-        c_turbine   = params['turbine_cost']
-        c_bos_turbine  = params['turbine_bos_costs']
-        c_opex_turbine = params['turbine_avg_annual_opex']
+        c_turbine   = params['turbine_cost'] 
+        c_bos_turbine  = params['turbine_bos_costs'] 
+        c_opex_turbine = params['turbine_avg_annual_opex'] 
         fcr         = params['fixed_charge_rate']
         tax         = params['tax_rate']
         r           = params['discount_rate']
@@ -47,6 +49,9 @@ class PlantFinance(Component):
         park_aep    = params['park_aep']
         t_construct = params['construction_time']
         t_project   = params['project_lifetime']
+        t_rating    = params['machine_rating']
+        npr         = params['turbine_number'] * params['machine_rating'] # net park rating, used in net energy capture calculation below
+        nec         = params['turbine_aep'] * params['turbine_number'] / (npr * 1.e003) # net energy rating, per COE report
         
         # Handy offshore boolean flag
         offshore = (depth > 0.0)
@@ -71,41 +76,40 @@ class PlantFinance(Component):
                 exit('ERROR: AEP is not connected properly. Both turbine_aep and park_aep are currently equal to 0 Wh. Check the connections to Plant_FinanceSE')
         
         
-        icc     = n_turbine * (c_turbine + c_bos_turbine)
-        c_opex  = n_turbine * c_opex_turbine
+        icc     = (c_turbine + c_bos_turbine) / (t_rating * 1.e003) #$/kW, changed per COE report
+        c_opex  = (c_opex_turbine) / (t_rating * 1.e003)  # $/kW, changed per COE report
+
         
         if offshore:
            # warranty Premium 
            icc += (c_turbine * n_turbine / 1.10) * 0.15
         
         #compute COE and LCOE values
-        unknowns['coe'] = (icc*fcr + c_opex*(1-tax)) / park_aep
+        unknowns['lcoe'] = ((icc * fcr + c_opex) / nec) # changed per COE report
 
-        self.amortFactor = (1. + 0.5 * ((1. + r)**t_construct - 1.)) * (r / (1. - (1. + r)**(-t_project)))
-        unknowns['lcoe'] = (icc * self.amortFactor + c_opex) / park_aep
-        
         if self.verbosity == True:
             print('################################################')
             print('Computation of CoE and LCoE from Plant_FinanceSE')
             print('Inputs:')
-            print('Water depth                      %.2f m'     % depth)
-            print('Number of turbines in the park   %u'         % n_turbine)
-            print('Cost of the single turbine       %.3f M USD' % (c_turbine * 1.e-006))  
-            print('BoS costs of the single turbine  %.3f M USD' % (c_bos_turbine * 1.e-006))  
-            print('Initial capital cost of the park %.3f M USD' % (icc * 1.e-006))  
-            print('Opex costs of the single turbine %.3f M USD' % (c_opex_turbine * 1.e-006))
-            print('Opex costs of the park           %.3f M USD' % (c_opex * 1.e-006))              
-            print('Fixed charge rate                %.2f %%'    % (fcr * 100.))     
-            print('Tax rate                         %.2f %%'    % (tax * 100.))        
-            print('Discount rate                    %.2f %%'    % (r * 100.))        
-            print('Wake loss factor                 %.2f %%'    % (wlf * 100.))         
-            print('AEP of the single turbine        %.3f GWh'   % (turb_aep * 1.e-006))    
-            print('AEP of the wind plant            %.3f GWh'   % (park_aep * 1.e-006))   
-            print('Construction time                %.2f yr'    % t_construct) 
-            print('Project lifetime                 %.2f yr'    % t_project)
+            print('Water depth                      %.2f m'          % depth)
+            print('Number of turbines in the park   %u'              % n_turbine)
+            print('Cost of the single turbine       %.3f M USD'      % (c_turbine * 1.e-006))  
+            print('BoS costs of the single turbine  %.3f M USD'      % (c_bos_turbine * 1.e-006))  
+            print('Initial capital cost of the park %.3f M USD'      % (icc * 1.e-006))  
+            print('Opex costs of the single turbine %.3f M USD'      % (c_opex_turbine * 1.e-006))
+            print('Opex costs of the park           %.3f M USD'      % (c_opex * 1.e-006))              
+            print('Fixed charge rate                %.2f %%'         % (fcr * 100.))     
+            print('Tax rate                         %.2f %%'         % (tax * 100.))        
+            print('Discount rate                    %.2f %%'         % (r * 100.))        
+            print('Wake loss factor                 %.2f %%'         % (wlf * 100.))         
+            print('AEP of the single turbine        %.3f GWh'        % (turb_aep * 1.e-006))    
+            print('AEP of the wind plant            %.3f GWh'        % (park_aep * 1.e-006))   
+            print('Construction time                %.2f yr'         % t_construct)
+            print('Project lifetime                 %.2f yr'         % t_project)
+            print('Capital costs                    %.2f $/kW'       % icc) #added
+            print('NEC                              %.2f MWh/MW/yr'  % nec) #added
             print('Outputs:')
-            print('CoE                              %.3f USD/MW' % (unknowns['coe']  * 1.e003))
-            print('LCoE                             %.3f USD/MW' % (unknowns['lcoe'] * 1.e003))
+            print('LCoE                             %.3f USD/MW'     % (unknowns['lcoe']  * 1.e003)) #removed "coe", best to have only one metric for cost
             print('################################################')
             
                     
@@ -163,6 +167,4 @@ class PlantFinance(Component):
         J['lcoe', 'net_aep'        ] = dlcoe_daep
         
         return J
-        
 
-        
